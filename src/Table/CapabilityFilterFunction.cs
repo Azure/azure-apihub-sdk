fe2573f21c4b29324a29b25bfa062d440dbc4137
Emil Cicos <emil.cicos@hotmail.com>
2016-07-07 16:25:34 -0700

    Additional filter functions supported by connectors.

--- src/Table/CapabilityFilterFunction.cs
+++ src/Table/CapabilityFilterFunction.cs
@@ -17,5 +17,51 @@
         And = Protocol.CapabilityFilterFunction.And,
 
         Or = Protocol.CapabilityFilterFunction.Or,
+
+        Contains = Protocol.CapabilityFilterFunction.Contains,
+
+        StartsWith = Protocol.CapabilityFilterFunction.StartsWith,
+
+        EndsWith = Protocol.CapabilityFilterFunction.EndsWith,
+
+        Length = Protocol.CapabilityFilterFunction.Length,
+
+        IndexOf = Protocol.CapabilityFilterFunction.IndexOf,
+
+        Replace = Protocol.CapabilityFilterFunction.Replace,
+
+        Substring = Protocol.CapabilityFilterFunction.Substring,
+
+        SubstringOf = Protocol.CapabilityFilterFunction.SubstringOf,
+
+        ToLower = Protocol.CapabilityFilterFunction.ToLower,
+
+        ToUpper = Protocol.CapabilityFilterFunction.ToUpper,
+
+        Trim = Protocol.CapabilityFilterFunction.Trim,
+
+        Concat = Protocol.CapabilityFilterFunction.Concat,
+
+        Year = Protocol.CapabilityFilterFunction.Year,
+
+        Month = Protocol.CapabilityFilterFunction.Month,
+
+        Day = Protocol.CapabilityFilterFunction.Day,
+
+        Hour = Protocol.CapabilityFilterFunction.Hour,
+
+        Minute = Protocol.CapabilityFilterFunction.Minute,
+
+        Second = Protocol.CapabilityFilterFunction.Second,
+
+        Floor = Protocol.CapabilityFilterFunction.Floor,
+
+        Ceiling = Protocol.CapabilityFilterFunction.Ceiling,
+
+        Round = Protocol.CapabilityFilterFunction.Round,
+
+        Not = Protocol.CapabilityFilterFunction.Not,
+
+        Negate = Protocol.CapabilityFilterFunction.Negate,
     }
 }
